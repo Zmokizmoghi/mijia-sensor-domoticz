3da66a775d84dd90aee509d35817053a05dfec23
Alexander Gornov <zmokizmoghi@gmail.com>
2018-12-09 13:59:10 +0800

    Testing

--- domoticz_mijia.py
+++ domoticz_mijia.py
@@ -20,16 +20,16 @@ domoticzpassword = ""
 # type temperature & humidity
 
 
-base64string = base64.encodestring(('%s:%s' % (domoticzusername, domoticzpassword)).encode()).decode().replace('\n', '')
+# base64string = base64.encodestring(('%s:%s' % (domoticzusername, domoticzpassword)).encode()).decode().replace('\n', '')
 
 def domoticzrequest (url):
   print(url)
-  request = urllib.request.Request(url)
-  request.add_header("Authorization", "Basic %s" % base64string)
-  response = urllib.request.urlopen(request)
-  return response.read()
+  # request = urllib.request.Request(url)
+  # request.add_header("Authorization", "Basic %s" % base64string)
+  # response = urllib.request.urlopen(request)
+  # return response.read()
 
-def update(address,idx_temp):
+def update(address):
 
     poller = MijiaPoller(address)
 
@@ -39,7 +39,7 @@ def update(address,idx_temp):
         temp = poller.parameter_value(MI_TEMPERATURE)
     except:
         temp = "Not set"
-    
+
     while loop < 2 and temp == "Not set":
         print("Error reading value retry after 5 seconds...\n")
         time.sleep(5)
@@ -49,11 +49,11 @@ def update(address,idx_temp):
             temp = poller.parameter_value(MI_TEMPERATURE)
         except:
             temp = "Not set"
-    
+
     if temp == "Not set":
         print("Error reading value\n")
         return
-    
+
     global domoticzserver
 
     print("Mi Sensor: " + address)
@@ -64,7 +64,7 @@ def update(address,idx_temp):
     print("Battery: {}%".format(poller.parameter_value(MI_BATTERY)))
 
     val_bat  = "{}".format(poller.parameter_value(MI_BATTERY))
-    
+
     # Update temp
     #val_temp = "{}".format(poller.parameter_value(MI_TEMPERATURE))
     #domoticzrequest("http://" + domoticzserver + "/json.htm?type=command&param=udevice&idx=" + idx_temp + "&nvalue=0&svalue=" + val_temp + "&battery=" + val_bat)
@@ -76,7 +76,7 @@ def update(address,idx_temp):
 	#/json.htm?type=command&param=udevice&idx=IDX&nvalue=0&svalue=TEMP;HUM;HUM_STAT
     val_temp = "{}".format(poller.parameter_value(MI_TEMPERATURE))
     val_hum = "{}".format(poller.parameter_value(MI_HUMIDITY))
-    
+
     val_comfort = "0"
     if float(val_hum) < 40:
         val_comfort = "2"
@@ -84,16 +84,12 @@ def update(address,idx_temp):
         val_comfort = "1"
     elif float(val_hum) > 70:
         val_comfort = "3"
-    
+
     domoticzrequest("http://" + domoticzserver + "/json.htm?type=command&param=udevice&idx=" + idx_temp + "&nvalue=0&svalue=" + val_temp + ";" + val_hum + ";"+ val_comfort + "&battery=" + val_bat)
-	
+
 
 print("\n1: updating")
-update("4C:65:A8:D0:4C:98","752")
-
-update("4C:65:A8:D0:26:D2","753")
-
-update("4C:65:A8:D0:57:2A","754")
+update("5F:BB:30:0B:C8:44")
 
 
 
